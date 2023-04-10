e57a2f3e966f7e45a6c4679b9a383d59cd2ded83
wfppfw <wfppfw12138@163.com>
2023-04-10 15:13:20 +0800

    fix: delete code

--- src/components/ip/index.vue
+++ src/components/ip/index.vue
@@ -23,67 +23,67 @@
 </template>
 <script lang="ts" setup>
 import axios from 'axios';
-import { jsonp } from 'vue-jsonp';
+// import { jsonp } from 'vue-jsonp';
 
 import { onMounted, ref } from 'vue';
 const ipInput = ref('');
-let xhr = new XMLHttpRequest();
-const url = 'https://gamematrix.qq.com/sdk/v2/get_gateway';
-xhr.open('GET', url, true);
+// let xhr = new XMLHttpRequest();
+// const url = 'https://gamematrix.qq.com/sdk/v2/get_gateway';
+// xhr.open('GET', url, true);
 
-xhr.setRequestHeader('Content-type', 'application/json');
-try {
-  xhr.send(); //改了这里
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      console.log(JSON.parse(xhr.responseText)); //输入相应的内容
-    }
-  };
-} catch (error) {
-  console.log(error);
-}
+// xhr.setRequestHeader('Content-type', 'application/json');
+// try {
+//   xhr.send(); //改了这里
+//   xhr.onreadystatechange = function () {
+//     if (xhr.readyState == 4 && xhr.status == 200) {
+//       console.log(JSON.parse(xhr.responseText)); //输入相应的内容
+//     }
+//   };
+// } catch (error) {
+//   console.log(error);
+// }
 
-const getIpadress = () => {
-  console.log('starts');
-  //   if (ipInput.value) {
-  console.log('starts2');
-  axios.get(url).then((res) => {
-    console.log(res);
-  });
-  //   }
-};
+// const getIpadress = () => {
+//   console.log('starts');
+//   //   if (ipInput.value) {
+//   console.log('starts2');
+//   axios.get(url).then((res) => {
+//     console.log(res);
+//   });
+//   //   }
+// };
 
-const Fn = (res) => {
-  console.log(res);
-};
-const addjsonp = (url, Fn) => {
-  let script = document.createElement('script');
-  script.src = url + `cb=${Fn.name}`;
-  document.body.appendChild(script);
-};
+// const Fn = (res) => {
+//   console.log(res);
+// };
+// const addjsonp = (url, Fn) => {
+//   let script = document.createElement('script');
+//   script.src = url + `cb=${Fn.name}`;
+//   document.body.appendChild(script);
+// };
 
-const getUserIp = async () => {
-  return new Promise((resolve, reject) => {
-    jsonp(
-      'https://apis.map.qq.com/ws/location/v1/ip?key=6CJBZ-EBHRH-I5BDY-WGC66-KN6OF-Y4FD5',
-      {
-        output: 'jsonp',
-        dataType: 'jsonp',
-      }
-    )
-      .then((res) => {
-        resolve(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  });
-};
+// const getUserIp = async () => {
+//   return new Promise((resolve, reject) => {
+//     jsonp(
+//       'https://apis.map.qq.com/ws/location/v1/ip?key=6CJBZ-EBHRH-I5BDY-WGC66-KN6OF-Y4FD5',
+//       {
+//         output: 'jsonp',
+//         dataType: 'jsonp',
+//       }
+//     )
+//       .then((res) => {
+//         resolve(res);
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   });
+// };
 
 onMounted(async () => {
-  addjsonp(url, Fn);
-  let res = await getUserIp();
-  console.log(res);
+  // addjsonp(url, Fn);
+  // let res = await getUserIp();
+  // console.log(res);
 });
 </script>
 
